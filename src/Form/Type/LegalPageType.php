eb96bbfd760417f44dbf6af71e96a9c9c6d8dc85
saslamme <moin@identsearch.de>
2026-08-28 14:58:49 +0200

    Fix legal page locale code mapping

--- src/Form/Type/LegalPageType.php
+++ src/Form/Type/LegalPageType.php
@@ -6,8 +6,10 @@ namespace App\Form\Type;
 
 use App\Entity\Content\LegalPage;
 use Sylius\Bundle\ChannelBundle\Form\Type\ChannelChoiceType;
-use Sylius\Bundle\LocaleBundle\Form\Type\LocaleChoiceType;
+use Sylius\Component\Locale\Model\LocaleInterface;
+use Sylius\Resource\Doctrine\Persistence\RepositoryInterface;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -15,6 +17,11 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 final class LegalPageType extends AbstractType
 {
+    /** @param RepositoryInterface<LocaleInterface> $localeRepository */
+    public function __construct(private readonly RepositoryInterface $localeRepository)
+    {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -22,9 +29,10 @@ final class LegalPageType extends AbstractType
                 'label' => 'Typ (interner Code)',
                 'help' => 'Zum Beispiel imprint, privacy oder terms.',
             ])
-            ->add('localeCode', LocaleChoiceType::class, [
+            ->add('localeCode', ChoiceType::class, [
                 'label' => 'Sprache',
-                'multiple' => false,
+                'choices' => $this->localeChoices(),
+                'choice_translation_domain' => false,
             ])
             ->add('channels', ChannelChoiceType::class, [
                 'label' => 'Verkaufskanäle',
@@ -59,5 +67,22 @@ final class LegalPageType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults(['data_class' => LegalPage::class]);
+    }
+
+    /** @return array<string, string> */
+    private function localeChoices(): array
+    {
+        $choices = [];
+
+        foreach ($this->localeRepository->findAll() as $locale) {
+            $code = $locale->getCode();
+            $name = $locale->getName();
+
+            if ($code !== null && $name !== null) {
+                $choices[$name] = $code;
+            }
+        }
+
+        return $choices;
     }
 }
