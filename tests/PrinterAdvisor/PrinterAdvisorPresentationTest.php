b634886fde5f7135d1841bc46114eba41d5d71ed
saslamme <moin@identsearch.de>
2026-09-02 17:05:53 +0200

    Fix cropped printer advisor product images

--- tests/PrinterAdvisor/PrinterAdvisorPresentationTest.php
+++ tests/PrinterAdvisor/PrinterAdvisorPresentationTest.php
@@ -15,14 +15,15 @@ final class PrinterAdvisorPresentationTest extends TestCase
         $stylesheet = file_get_contents($root.'/assets/shop/styles/cardnext.css');
 
         self::assertIsString($template);
-        self::assertStringContainsString('<div class="cn-advisor__image">', $template);
-        self::assertStringContainsString("class: 'cn-advisor__product-image'", $template);
+        self::assertStringContainsString('<div class="cn-advisor__media">', $template);
+        self::assertStringContainsString('class="cn-advisor__product-image"', $template);
+        self::assertStringContainsString("recommendationImage.path|imagine_filter('cardnext_product_card')", $template);
         self::assertStringContainsString('class="cn-advisor__image-placeholder"', $template);
-        self::assertStringNotContainsString("class: 'img-fluid', filter: 'cardnext_product_card'", $template);
+        self::assertStringNotContainsString("component('sylius_shop:main_image'", $template);
 
         self::assertIsString($stylesheet);
-        self::assertMatchesRegularExpression('/\\.cn-advisor__image \\{[^}]*height: 190px;[^}]*overflow: hidden;[^}]*place-items: center;/s', $stylesheet);
-        self::assertMatchesRegularExpression('/\\.cn-advisor__product-image \\{[^}]*width: 100% !important;[^}]*height: 100% !important;[^}]*object-fit: contain !important;[^}]*object-position: center !important;/s', $stylesheet);
+        self::assertMatchesRegularExpression('/\\.cn-advisor__media \\{[^}]*height: 220px;[^}]*overflow: hidden;[^}]*align-items: center;[^}]*justify-content: center;/s', $stylesheet);
+        self::assertMatchesRegularExpression('/\\.cn-advisor__product-image \\{[^}]*position: static !important;[^}]*width: 100% !important;[^}]*height: 100% !important;[^}]*object-fit: contain !important;[^}]*object-position: center !important;/s', $stylesheet);
         self::assertStringContainsString('.cn-advisor__image-placeholder', $stylesheet);
     }
 }
