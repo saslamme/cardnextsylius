54e82a960b1678844055f24ef55f01d343fea351
saslamme <moin@identsearch.de>
2026-09-02 20:59:59 +0200

    Refine homepage tool promo cards

--- tests/Branding/HomepagePromoTemplateTest.php
+++ tests/Branding/HomepagePromoTemplateTest.php
@@ -12,11 +12,23 @@ final class HomepagePromoTemplateTest extends TestCase
     {
         $homepage = (string) file_get_contents(__DIR__ . '/../../templates/bundles/SyliusShopBundle/homepage/index.html.twig');
         $promo = (string) file_get_contents(__DIR__ . '/../../templates/shop/homepage/_promos.html.twig');
+        $stylesheet = (string) file_get_contents(__DIR__ . '/../../assets/shop/styles/cardnext.css');
 
         self::assertGreaterThan(strpos($homepage, 'id="products"'), strpos($homepage, 'shop/homepage/_promos.html.twig'));
         self::assertStringContainsString('promo.enabled', $promo);
         self::assertStringContainsString('promo.imagePath', $promo);
         self::assertStringContainsString('promo.imageAlt', $promo);
+        self::assertStringContainsString('promo.url', $promo);
+        self::assertStringContainsString('promo.buttonLabel', $promo);
+        self::assertStringContainsString('promo.badge', $promo);
+        self::assertStringContainsString('cn-home-promo__media', $promo);
+        self::assertStringContainsString('cn-home-promo__image', $promo);
         self::assertStringNotContainsString('Kartendrucker', $promo);
+
+        self::assertMatchesRegularExpression('/\\.cn-home-promo__media \\{[^}]*display: grid;[^}]*place-items: center;[^}]*overflow: hidden;/s', $stylesheet);
+        self::assertMatchesRegularExpression('/\\.cn-home-promo__image \\{[^}]*object-fit: contain;[^}]*object-position: center;/s', $stylesheet);
+        self::assertDoesNotMatchRegularExpression('/\\.cn-home-promo(?:__media(?: img)?|__image) \\{[^}]*object-fit: cover;/s', $stylesheet);
+        self::assertStringContainsString('@media (max-width: 1050px) { .cn-home-promos__grid { grid-template-columns: minmax(0,1fr); } }', $stylesheet);
+        self::assertStringContainsString('.cn-home-promo--text-only { grid-template-columns: minmax(0,1fr); }', $stylesheet);
     }
 }
