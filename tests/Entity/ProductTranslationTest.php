6711af0fe13c8e96f2ac2cda396e5937b0195cc7
saslamme <moin@identsearch.de>
2026-09-09 15:56:26 +0200

    Fix product search synonyms column mapping

--- tests/Entity/ProductTranslationTest.php
+++ tests/Entity/ProductTranslationTest.php
@@ -5,10 +5,20 @@ declare(strict_types=1);
 namespace Tests\Entity;
 
 use App\Entity\Product\ProductTranslation;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\Mapping\Driver\AttributeDriver;
 use PHPUnit\Framework\TestCase;
 
 final class ProductTranslationTest extends TestCase
 {
+    public function testSearchSynonymsUsesMigrationColumnName(): void
+    {
+        $metadata = new ClassMetadata(ProductTranslation::class);
+        (new AttributeDriver([\dirname(__DIR__, 2) . '/src/Entity/Product']))->loadMetadataForClass(ProductTranslation::class, $metadata);
+
+        self::assertSame('search_synonyms', $metadata->getColumnName('searchSynonyms'));
+    }
+
     public function testSearchSynonymsPreserveTheirFormattingAndTrimOuterWhitespace(): void
     {
         $translation = new ProductTranslation();
